a6a6a5b1e83143e553cefe8dffa35dc12d714f07
datdh <luongquanghai92@gmail.com>
2013-04-27 05:09:15 +0700

    Menu screen update

---
+++ WhatCanWeCook/WhatCanWeCook/AddARecipeWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddARecipeWebViewController.h
+//  WhatCanWeCook
+//
+//  Created by hust9 on 4/27/13.
+//  Copyright (c) 2013 UIHUST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddARecipeWebViewController : UIViewController
+
+@end
