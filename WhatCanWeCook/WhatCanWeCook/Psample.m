0c67e26c4a099066a9279e0a0e8ece56df512474
SAMEER UPASANI <sameer-upasani@uiowa.edu>
2013-04-26 01:09:29 -0500

    modified sample.m file

--- WhatCanWeCook/WhatCanWeCook/Psample.m
+++ WhatCanWeCook/WhatCanWeCook/Psample.m
@@ -4,7 +4,7 @@
 //
 //  Created by uicsi5 on 4/6/13.
 //  Copyright (c) 2013 uicsi5. All rights reserved.
-//added some files
+//modified this file
 
 #import "Psample.h"
 #import "Precipe.h"
@@ -16,7 +16,7 @@ UIImage *img1, *img2, *img3;
 
 - (void)viewDidLoad
 {
-
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     // warning here, but it allows the picture to appear
@@ -36,11 +36,11 @@ UIImage *img1, *img2, *img3;
     img3 = [UIImage imageNamed:@"sbcrepe.jpg"];
     [imageview3 setImage:img3];
     
-    label1.text = @"2 Egg, 2 Tbsp. whole milk, 2 Tbsp clarified butter, Salt and white paper ";
+    label1.text = @"Omlette";
     
-    label2.text = @"1 1/2 pounds ground chuck Kosher salt and black pepper, 1 1/2 tablespoons canola oil, 4 slices cheese, 4 hamburger burns, split";
+    label2.text = @"Burger";
     
-    label3.text = @"1 egg, beaten, 1/4 cup skim milk, 1/3 cup water, 1/2 cup semisweet chocolate......   ";
+    label3.text = @"Stawberry Crepe";
     
 }
 
@@ -57,11 +57,11 @@ UIImage *img1, *img2, *img3;
     
     
     
-    label3.text = @"2 Egg                                      2 Tbsp. whole milk                  2 Tbsp clarified butter           Salt and white paper ";
+    label3.text = @"Omlette";
     
-    label2.text = @"1 egg, beaten                         1/4 cup skim milk                  1/3 cup water                        1/2 cup semisweet chocolate......   ";
+    label2.text = @"Stawberry Crepe";
     
-    label1.text = @"1 1/2 pounds ground chuck Kosher salt and black pepper 1 1/2 tablespoons canola oil    4 slices cheese                        4 hamburger burns, split";
+    label1.text = @"Burger";
     
 }
 -(IBAction)show3 {
@@ -75,11 +75,11 @@ UIImage *img1, *img2, *img3;
     img3 = [UIImage imageNamed:@"Giant_Burger_frei.jpg"];
     [imageview3 setImage:img3];
     
-    label2.text = @"2 Egg                                      2 Tbsp. whole milk                  2 Tbsp clarified butter           Salt and white paper ";
+    label2.text = @"Omlette";
     
-    label1.text = @"1 egg, beaten                         1/4 cup skim milk                  1/3 cup water                        1/2 cup semisweet chocolate......   ";
+    label1.text = @"Stawberry Crepe";
     
-    label3.text = @"1 1/2 pounds ground chuck Kosher salt and black pepper 1 1/2 tablespoons canola oil    4 slices cheese                        4 hamburger burns, split";
+    label3.text = @"Burger";
 }
 
 
@@ -87,16 +87,16 @@ UIImage *img1, *img2, *img3;
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
     if([segue.identifier isEqualToString:@"push1"]){
         
-
-         
+        
+        
         Precipe *p1 = [segue destinationViewController];
         
         p1.img = img1;
-         
+        
         s1 = label1.text;
         p1.introString = s1;
     } else if([segue.identifier isEqualToString:@"push2"]){
-       
+        
         Precipe *p1 = [segue destinationViewController];
         
         p1.img = img2;
@@ -105,7 +105,7 @@ UIImage *img1, *img2, *img3;
         
         
     } else if([segue.identifier isEqualToString:@"push3"]){
-       
+        
         Precipe *p1 = [segue destinationViewController];
         
         p1.img = img3;
